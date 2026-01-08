981d0d443c2fe917d893853a06172ec84e21bd98
Mohammad Rizwan <m.shaik@serrala.com>
2026-01-08 10:01:53 +0000

    After event handler

---
+++ srv/CatalogService.js
@@ -0,0 +1,22 @@
+const cds = require('@sap/cds');
+
+class CatalogService extends cds.ApplicationService {
+
+    init() {
+        const { Books } = this.entities;
+
+        this.after(['READ'], Books, this.grantDiscount)
+
+        return super.init()
+    }
+
+    grantDiscount(results) {
+        for (let b of results) {
+            if (b.stock > 200) {
+                b.title += ' -- 11% Discount';
+            }
+        }
+    }
+}
+
+module.exports = CatalogService;
